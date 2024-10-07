89180e16c50b0cbc1ba4f07ea50a8e95d2cf3173
Niels Huisman <nielsz@pm.me>
2024-10-08 01:17:54 +0200

    bao

--- app/main.py
+++ app/main.py
@@ -14,7 +14,16 @@ if len(sys.argv)>=2:
 
 @Gooey(
     program_name="SWING MACHINE",
-    program_description="Niels Huisman, 2024",
+    program_description="Niels Huisman, 2024", 
+    image_dir="icons",
+    default_size=(750, 610),
+    #Example
+    # @Gooey(progress_regex=r"^progress: (?P<current>\d+)/(?P<total>\d+)$",
+    #    progress_expr="current / total * 100")
+    # match "[<current>/<total>]" in print statements to update progress bar
+    progress_regex=r"^\[(?P<current>\d+)/(?P<total>\d+)\]$",
+    progress_expr="current / total * 100",
+    hide_progress_msg=True,
 )
 def main():
     # parser = argparse.ArgumentParser(description="Swing Machine: Add swing to your audio files.")
@@ -26,19 +35,25 @@ def main():
     parser = GooeyParser(prog="swing", description="SWING MACHINE.\nNiels Huisman, 2024")
     parser.add_argument('-i', '--input-file', metavar="Input", help='Input audio file', widget='FileChooser', required=True)
     parser.add_argument('-o', '--output-file', metavar="Output", help='Output audio file. (optional)\nDefaults to <inputfile>_swing.wav', widget='FileSaver')
+    parser.add_argument('--halftime', metavar="Halftime", help='Use halftime swing.', action='store_true')
     debug = parser.add_argument_group('Debug zooi')
 
     debug.add_argument('-c', '--produce-click-track', metavar="Produce clicktrack", action='store_true', help='Produce a click track as well. Useful for debugging.')
     args = parser.parse_args()
 
     PRODUCE_CLICK_TRACK_AS_WELL = args.produce_click_track
-    INFILE = args.infile
-    OUTFILE = args.outfile
+    INFILE = args.input_file
+    OUTFILE = args.output_file if args.output_file else INFILE.replace(".wav", "_swing.wav")
+    if not INFILE.endswith(".wav"):
+        print("Input file must be a .wav file")
+        sys.exit(1)
     
     print("Welcome to SWING MACHINE")
-
+    
+    print("[0/3]")
     print("Loading file...")
     y, sr = librosa.load(INFILE, sr=None)
+    print("[1/3]")
     print("Detecting beats...")
     tempo, beats = librosa.beat.beat_track(y=y, sr=sr, units='samples')
 
@@ -48,7 +63,7 @@ def main():
         y_click = y + click
         sf.write(OUTFILE.replace(".wav", "_click.wav"), y_click, sr)
 
-
+    print("[2/3]")
     print("Swinging....")
     y_new = []
     for i in range(len(beats)-1):
@@ -60,7 +75,8 @@ def main():
 
         y_new.extend(first_half)
         y_new.extend(second_half)
-
+    
+    print("[3/3]")
     print("Saving file...")
     # Save the new audio file
     sf.write(OUTFILE, y_new, sr)
